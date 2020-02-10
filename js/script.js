31c902de4fa51d902de49213ded046ce0265b284
serendipitypunch <michaeltsherry@gmail.com>
2020-02-09 22:14:09 -0800

    Added Quotes

--- js/script.js
+++ js/script.js
@@ -10,7 +10,45 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
+var quotes [
+  {
+      quote: "Nothing is original. Steal from anywhere that resonates with inspiration or fuels your imagination. Devour old films, new films, music, books, paintings, photographs, poems, dreams, random conversations, architecture, bridges, street signs, trees, clouds, bodies of water, light and shadows. Select only things to steal from that speak directly to your soul. If you do this, your work (and theft) will be authentic. Authenticity is invaluable; originality is non-existent. And don’t bother concealing your thievery - celebrate it if you feel like it. In any case, always remember what Jean-Luc Godard said: “It’s not where you take things from - it’s where you take them to.",
+      source: "Jim Jarmusch",
+      citation: "MovieMaker Magazine #53",
+      year: "Winter, January 22, 2004"   
+  },
+  {
+      quote: "“And one day we must ask the question, ‘Why are there forty million poor people in America? And when you begin to ask that question, you are raising questions about the economic system, about a broader distribution of wealth.’ When you ask that question, you begin to question the capitalistic economy. And I’m simply saying that more and more, we’ve got to begin to ask questions about the whole society…”",
+      source: "Martin Luther King Jr.",
+      citation: "Speech to Southern Christian Leadership Conference Atlanta, Georgia",
+      year: "August 16, 1967"   
+  },
+  {
+      quote: "I maintain that Truth is a pathless land, and you cannot approach it by any path whatsoever, by any religion, by any sect. That is my point of view, and I adhere to that absolutely and unconditionally. Truth, being limitless, unconditioned, unapproachable by any path whatsoever, cannot be organized; nor should any organization be formed to lead or to coerce people along any particular path. If you first understand that, then you will see how impossible it is to organize a belief. A belief is purely an individual matter, and you cannot and must not organize it. If you do, it becomes dead, crystallized; it becomes a creed, a sect, a religion, to be imposed on others. This is what everyone throughout the world is attempting to do. Truth is narrowed down and made a plaything for those who are weak, for those who are only momentarily discontented. Truth cannot be brought down, rather the individual must make the effort to ascend to it. You cannot bring the mountain-top to the valley. If you would attain to the mountain-top you must pass through the valley, climb the steeps, unafraid of the dangerous precipices.",
+      source: "Jiddu Krishnamurti",
+      citation: "Truth is a Pathless Land",
+      year: "1929"   
+  },
+  {
+      quote: "Trees are poems the earth writes upon the sky, We fell them down and turn them into paper\,
+      That we may record our emptiness.",
+      source: "Kahlil Gibran",
+      citation: "The Prophet",
+      year: "1923"   
+  },
+  {
+      quote: "“We seldom realize, for example that our most private thoughts and emotions are not actually our own. For we think in terms of languages and images which we did not invent, but which were given to us by our society.”",
+      source: "Alan W. Watts",
+      citation: "The Book on the Taboo Against Knowing Who You Are",
+      year: "1966"   
+  },
+  {
+      quote: "I have found that it is the small everyday deed of ordinary folks that keep the darkness at bay. Small acts of kindness and love.",
+      source: "Gandalf",
+      citation: "J. R. R. Tolkein ~ The Hobbit",
+      year: "September 21, 1937"   
+  }
+]
 
 
 /***

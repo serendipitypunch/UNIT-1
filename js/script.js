5efb0dcc8bc664726cbbff8d8d3c6dc0c95ee7e6
serendipitypunch <michaeltsherry@gmail.com>
2020-02-27 15:56:12 -0800

    Finished but needs testing

--- js/script.js
+++ js/script.js
@@ -7,37 +7,38 @@ project 1 - A Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
+
 /*** 
- * `quotes` array 
+ * Creates the quotes array
+ * Creates 5 objects inside of the array
 ***/
-var quotes [
+var quotes = [
   {
-      quote: "Nothing is original. Steal from anywhere that resonates with inspiration or fuels your imagination. Devour old films, new films, music, books, paintings, photographs, poems, dreams, random conversations, architecture, bridges, street signs, trees, clouds, bodies of water, light and shadows. Select only things to steal from that speak directly to your soul. If you do this, your work (and theft) will be authentic. Authenticity is invaluable; originality is non-existent. And don’t bother concealing your thievery - celebrate it if you feel like it. In any case, always remember what Jean-Luc Godard said: “It’s not where you take things from - it’s where you take them to.",
+      quote: "Nothing is original. Steal from anywhere that resonates with inspiration or fuels your imagination.",
       source: "Jim Jarmusch",
       citation: "MovieMaker Magazine #53",
       year: "Winter, January 22, 2004"   
   },
   {
-      quote: "“And one day we must ask the question, ‘Why are there forty million poor people in America? And when you begin to ask that question, you are raising questions about the economic system, about a broader distribution of wealth.’ When you ask that question, you begin to question the capitalistic economy. And I’m simply saying that more and more, we’ve got to begin to ask questions about the whole society…”",
-      source: "Martin Luther King Jr.",
+      quote: "And one day we must ask the question, ‘Why are there forty million poor people in America? And when you begin to ask that question, you are raising questions about the economic system, about a broader distribution of wealth.",
+      source: "Martin Luther King Jr",
       citation: "Speech to Southern Christian Leadership Conference Atlanta, Georgia",
       year: "August 16, 1967"   
   },
   {
-      quote: "I maintain that Truth is a pathless land, and you cannot approach it by any path whatsoever, by any religion, by any sect. That is my point of view, and I adhere to that absolutely and unconditionally. Truth, being limitless, unconditioned, unapproachable by any path whatsoever, cannot be organized; nor should any organization be formed to lead or to coerce people along any particular path. If you first understand that, then you will see how impossible it is to organize a belief. A belief is purely an individual matter, and you cannot and must not organize it. If you do, it becomes dead, crystallized; it becomes a creed, a sect, a religion, to be imposed on others. This is what everyone throughout the world is attempting to do. Truth is narrowed down and made a plaything for those who are weak, for those who are only momentarily discontented. Truth cannot be brought down, rather the individual must make the effort to ascend to it. You cannot bring the mountain-top to the valley. If you would attain to the mountain-top you must pass through the valley, climb the steeps, unafraid of the dangerous precipices.",
+      quote: "I maintain that Truth is a pathless land, and you cannot approach it by any path whatsoever, by any religion, by any sect.",
       source: "Jiddu Krishnamurti",
       citation: "Truth is a Pathless Land",
       year: "1929"   
   },
   {
-      quote: "Trees are poems the earth writes upon the sky, We fell them down and turn them into paper\,
-      That we may record our emptiness.",
+      quote: "Trees are poems the earth writes upon the sky, We fell them down and turn them into paper, That we may record our emptiness.",
       source: "Kahlil Gibran",
       citation: "The Prophet",
       year: "1923"   
   },
   {
-      quote: "“We seldom realize, for example that our most private thoughts and emotions are not actually our own. For we think in terms of languages and images which we did not invent, but which were given to us by our society.”",
+      quote: "We seldom realize, for example that our most private thoughts and emotions are not actually our own. For we think in terms of languages and images which we did not invent, but which were given to us by our society.",
       source: "Alan W. Watts",
       citation: "The Book on the Taboo Against Knowing Who You Are",
       year: "1966"   
@@ -48,19 +49,53 @@ var quotes [
       citation: "J. R. R. Tolkein ~ The Hobbit",
       year: "September 21, 1937"   
   }
-]
+];
+
+
+// Creates empty variable to hold the output html
+let html;
 
 
 /***
- * `getRandomQuote` function
-***/
+ * Creates getRandomQuote function
+ * Generates a random number
+ * Uses the random number to access a randome object inside of the qoutes *array and return it.
+ ***/
 
+function getRandomQuote() {
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+    return quotes[randomNumber];
+}
 
 
 /***
- * `printQuote` function
+ * Creates printQuote function
+ * Stores the returned object from the getRandomQuote variable currentQuote
+ * Creates a concatenating html from the value of quote and source keys
+ * uses if statemnts to check if there are citation and year values
+ * If so adds concatenates them into the html varaible
+ * finally, adds a closing </p> tag and returns the html variable
 ***/
 
+function printQuote() {
+    let currentQuote = getRandomQuote();
+    let concHtml = '<p class="quote">' + currentQuote["quote"] + "</p>";
+    concHtml += '<p class="source">' + currentQuote["source"];
+    if (currentQuote.citation !== "") {
+        concHtml += '<span class="citation">' + currentQuote["citation"] + '</span>';
+    }
+    if (currentQuote.year !== "") {
+        concHtml += '<span class="year">' + currentQuote["year"] + '</span>';
+    }
+    concHtml += '</p>';
+    return concHtml;
+}
+
+// Stores the returned value of the printQuote function in html varable
+html = printQuote();
+
+// Displays the content of the html variable in the quote-box element
+document.getElementById('quote-box').innerHTML = html; 
 
 
 /***

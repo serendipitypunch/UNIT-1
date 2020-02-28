4dbb36b37fba1cceeadb3088cd66384196714c75
serendipitypunch <michaeltsherry@gmail.com>
2020-02-27 20:56:25 -0800

    Ready for peer review
    Finished and ready for review

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 
-/*** 
+/*** QUOTES ARRAY OF OBJECTS
  * Creates the quotes array
  * Creates 5 objects inside of the array
 ***/
@@ -13,68 +13,110 @@ var quotes = [
       quote: "Nothing is original. Steal from anywhere that resonates with inspiration or fuels your imagination.",
       source: "Jim Jarmusch",
       citation: "MovieMaker Magazine #53",
-      year: "Winter, January 22, 2004"   
+      year: "Winter, January 22, 2004",
+      tags: "Inspirational"   
   },
   {
-      quote: "And one day we must ask the question, ‘Why are there forty million poor people in America? And when you begin to ask that question, you are raising questions about the economic system, about a broader distribution of wealth.",
-      source: "Martin Luther King Jr",
-      citation: "Speech to Southern Christian Leadership Conference Atlanta, Georgia",
-      year: "August 16, 1967"   
-  },
-  {
-      quote: "I maintain that Truth is a pathless land, and you cannot approach it by any path whatsoever, by any religion, by any sect.",
+      quote: "The ability to observe without evaluating is the highest form of intelligence.",
       source: "Jiddu Krishnamurti",
-      citation: "Truth is a Pathless Land",
-      year: "1929"   
+      citation: "Think on These Things",
+      year: "October 11th 1989",
+      tags: "Spiritual"   
+  },
+  {
+      quote: "“Truth is in everyone; it is not far, it is not near; it is eternally there.",
+      source: "Jiddu Krishnamurti",
+      citation: "Total Freedom: The Essential Krishnamurti",
+      year: "1996",
+      tags: "Spiritual" 
   },
   {
       quote: "Trees are poems the earth writes upon the sky, We fell them down and turn them into paper, That we may record our emptiness.",
       source: "Kahlil Gibran",
       citation: "The Prophet",
-      year: "1923"   
+      year: "1923",
+      tags: "Spiritual, Poetry"    
   },
   {
-      quote: "We seldom realize, for example that our most private thoughts and emotions are not actually our own. For we think in terms of languages and images which we did not invent, but which were given to us by our society.",
+      quote: 'Only words and conventions can isolate us from the entirely undefinable something which is everything.',
       source: "Alan W. Watts",
-      citation: "The Book on the Taboo Against Knowing Who You Are",
-      year: "1966"   
+      citation: "The Wisdom of Insecurity",
+      year: "September 12th 1968",
+      tags: ""   
   },
+  {
+    quote: 'Those who know don’t talk. Those who talk don’t know.',
+    source: "Lao Tzu",
+    citation: "Tao Te Ching",
+    year: "",
+    tags: ""   
+  },
+  {
+    quote: 'Meditation is the discovery that the point of life is always arrived at in the immediate moment.',
+    source: "Alan W. Watts",
+    citation: "",
+    year: "",
+    tags: ""   
+},
   {
       quote: "I have found that it is the small everyday deed of ordinary folks that keep the darkness at bay. Small acts of kindness and love.",
       source: "Gandalf",
       citation: "J. R. R. Tolkein ~ The Hobbit",
-      year: "September 21, 1937"   
+      year: "September 21, 1937",
+      tags: "Fantasy Literature"   
   }
 ];
 
-
+// VARIABLES
 // Creates empty variable to hold the output html
 let html = "";
+let rgbColor;
+const rgbNum = 256;
 
-/***
+/*** RANDOM NUMBER GENERATOR
+ * A function that generates a random number from given argument
+ ***/
+function randomNumGen(num){
+    return Math.floor(Math.random() * num );
+}
+
+/*** RANDOM COLOR GENERATOR
+ * A function that generates a random rgb color and returns it
+***/
+function getRandomColor() {
+    let color = 'rgb(';
+    color += randomNumGen(rgbNum) + ',';
+    color += randomNumGen(rgbNum) + ',';
+    color += randomNumGen(rgbNum) + ')';
+    return color;
+  }
+
+/*** RANDOM QUOTE FUNCTION
  * Creates getRandomQuote function
  * Generates a random number
  * Uses the random number to access a random object inside of the quotes 
  * array and return it.
  ***/
 function getRandomQuote() {
-    let randomNumber = Math.floor(Math.random() * quotes.length);
+    let randomNumber = randomNumGen(quotes.length);
     return quotes[randomNumber];
 }
 
-
-/***
+/*** PRINT QUOTE FUNCTION
  * Creates printQuote function
- * Stores the returned object from the getRandomQuote variable currentQuote
+ * Stores the returned object from the getRandomQuote in variable currentQuote
+ * Stores the returned value from getRandomNumber in variable currentColor
  * Creates a concatenated html from the value of quote and source keys
- * Uses if statements to check if there are citation and year values
- * If so adds concatenates them into the html variable
+ * Uses if statements to check if there are citation, year and tag values
+ * If so concatenates them into the html variable
  * Adds a closing </p> tag
+ * Applies value of currentColor variable to document body background property
  * Sends the html variable to the provided code snippet to be displayed
  * in the DIV with the id of "quote-box"
 ***/
 function printQuote() {
     let currentQuote = getRandomQuote();
+    let currentColor = getRandomColor();
     html = '<p class="quote">' + currentQuote["quote"] + "</p>";
     html += '<p class="source">' + currentQuote["source"];
     if (currentQuote.citation !== "") {
@@ -83,16 +125,26 @@ function printQuote() {
     if (currentQuote.year !== "") {
         html += '<span class="year">' + currentQuote["year"] + '</span>';
     }
+    if (currentQuote.tags !== "") {
+        html += '<span class="tags">' + "<br>" + currentQuote["tags"] + '</span>';
+    }
     html += '</p>';
+    document.body.style.backgroundColor = currentColor;
     document.getElementById('quote-box').innerHTML = html;
 }
 
-// Stores the returned value of the printQuote function in html variable
-//getRandomQuote();
+
+// Stores value of the printQuote function in html variable
 html = printQuote();
+
 
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+
+/***
+ * Calls the printQuote function every 7 seconds
+***/
+setInterval(printQuote, 7000);
